0ca9ad1cb10dcc22e78347e93efbe84d7aefcc21
janWICKEDpro <janroyal45@gmail.com>
2024-01-27 06:07:52 +0100

    faq-accordion: completed design and js

---
+++ faq-accordion-main/script.js
@@ -0,0 +1,17 @@
+const questionDivs = document.querySelectorAll('.question');
+
+questionDivs.forEach(questionDiv =>{
+
+    let answer = questionDiv.nextElementSibling;
+    let plusIcon = questionDiv.querySelector('.plus');
+    let minusIcon = questionDiv.querySelector('.minus');
+
+    questionDiv.addEventListener('click', ()=>{
+        console.log('clicking');
+        console.log(answer.classList);
+        answer.classList.toggle('inactive');
+        plusIcon.classList.toggle('inactive');
+        minusIcon.classList.toggle('inactive');
+    })
+
+});
